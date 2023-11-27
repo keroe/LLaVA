f1b77e5efbbd0b65562d84c1b0efc41826e479f1
Kevin <kevin.roesch@kit.edu>
2023-11-27 14:35:23 +0100

    add function to loop over folder with images

--- playground/wrapper_example.py
+++ playground/wrapper_example.py
@@ -1,7 +1,23 @@
 from llava.model.llava_wrapper import LlavaWrapper
+from pathlib import Path
+from tqdm import tqdm
+import yaml
+import argparse
 
 if __name__ == "__main__":
-	llava_wrapper = LlavaWrapper("liuhaotian/llava-v1.5-7b")
-	image_file ="https://llava-vl.github.io/static/images/view.jpg"
-	query = "What can you see in the image?"
-	output = llava_wrapper.run(query, image_file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base-folder", type=str)
+    llava_wrapper = LlavaWrapper("liuhaotian/llava-v1.5-7b")
+    query = "Can you tell me which state the traffic light has? Possible states can be off, red, yellow, green. Please only answer with one word, the corresponding state."
+    args = parser.parse_args()
+    base_folder = Path(args.base_folder)
+    tl_dir = {"red": {"files": [], "labels": []}, "green": {"files": [], "labels": []}, "yellow": {"files": [], "labels": []}}
+    for color in ["red", "yellow", "green"]:
+            tl_dir[color]["files"] = [str(x) for x in (base_folder / Path(color)).glob("*.png")]
+
+    for color in ["red", "yellow", "green"]:
+            for file in tqdm(tl_dir[color]["files"]):
+                    tl_dir[color]["labels"].append(llava_wrapper.run(query, str(file)))
+
+    with open("/tmp/tl_crops_label.yaml", "w") as fi:
+            yaml.dump(tl_dir, fi)
